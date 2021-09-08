1ec2d6d87da6da62a5a630285938f4ea035d15e3
Bryan Serna Osorio <bryanseros@gmail.com>
2021-09-08 07:59:41 -0500

    enviar datos a la vista desde el controlador

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,12 +1,17 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
     @GetMapping({"/index","/", "home"})
-    public String index(){
+    public String index(ModelMap model){
+        model.addAttribute("titulo","Hola spring framework!");
         return "index";
     }
 }
